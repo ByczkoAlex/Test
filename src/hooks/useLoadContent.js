c608b81b9597d617d5bea3dc2be5c8f1d38f1660
Alex <byczko2012@mail.ru>
2021-10-20 13:40:54 +0300

    Added try/catch

--- src/hooks/useLoadContent.js
+++ src/hooks/useLoadContent.js
@@ -6,20 +6,29 @@ export const useLoadContent = () => {
     const [next, setNext] = useState([]);
     const [pageCount, SetPageCount] = useState(1);
     const getContent = useCallback(async (value) => {
-        await axios.get(`https://rickandmortyapi.com/api/character/?name=` + value).then(response => {
-            setImgList(response.data.results)
-            setNext(response.data.info.next)
-        })
+        try {
+            await axios.get(`https://rickandmortyapi.com/api/character/?name=` + value).then(response => {
+                setImgList(response.data.results)
+                setNext(response.data.info.next)
+            })
+        } catch (e) {
+            alert('error :  Invalid Name')
+        }
     }, []);
     // TODO: Put fetchMore method here
+
     const fetchMore = useCallback(async () => {
-        await axios.get(next)
-            .then(response => {
+        try {
+            await axios.get(next).then(response => {
                 setNext(response.data.info.next)
                 setImgList(prevState => [...prevState, ...response.data.results])
-                SetPageCount(prevState => prevState + 1 )
-                })
+                SetPageCount(prevState => prevState + 1)
+            })
+        } catch (e) {
+            alert('error :  Click submit button to fetch more')
+        }
     }, [next]);
+
     const ResultArray = imgList.filter((el, index) =>
         index < 10 * pageCount
     )
